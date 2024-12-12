91f0d00df753381eaab2885a8bb0eece35348beb
Caleb Dykman <z3460382@ad.unsw.edu.au>
2024-12-12 12:17:17 +1100

    new daily nearby station selection

--- src/pyraingen/regionaliseddailysim.py
+++ src/pyraingen/regionaliseddailysim.py
@@ -1,18 +1,21 @@
 import numpy as np
+import pandas as pd
+import xarray as xr
 import os
 
 from .fortran_daily import regionalised_dailyT
 from .get_fortran_data import copy_fortran_data
 from .convert_daily_to_NetCDF import convertdailync
+from .getnearbystations import station
 
 def regionaliseddailysim(nyears, startyear, nsim,
                             targetidx, targetlat, targetlon, 
                             targetelev, targetdcoast, targetanrf,
                             targettemp, data_path, output_path_txt, 
-                            netcdf = True, output_path_nc='daily.nc',
+                            output_path_nc='daily.nc',
                             output_stats="stat_.out", output_val="rev_.out",
                             cutoff=0.30, wind=15, nstation=5, 
-                            nlon=3, lag=1, iamt=1, ival=0, irf=1):
+                            nlon=3, lag=1, iamt=1, ival=0, irf=1, rm=1):
     """Front end to the regionalised daily rainfall generator.
     For a detailed description of the approach refer to: 
     Mehrotra et al (2012) "Continuous Rainfall simulation: 2.
@@ -96,28 +99,30 @@ def regionaliseddailysim(nyears, startyear, nsim,
     # Check if file exists
     if os.path.exists("drop.out"):
         os.remove("drop.out")
+    if os.path.exists("nearby_station_details.out"):
+        os.remove("nearby_station_details.out")
     
     # Input parameters
-    rain = str(cutoff) + ' '
-    iband = str(wind)  + ' '
+    rain     = str(cutoff) + ' '
+    iband    = str(wind)  + ' '
     nstation = str(nstation)  + ' ' 
-    nsim = str(nsim)  + ' '
-    nlon = str(nlon) + ' ' 
-    lag = str(1) + ' '
-    ng = str(nyears) + ' '
-    nsgtart = str(startyear) + ' '
-    iamt = str(iamt) + ' '
-    ival = str(ival) + ' '
-    irf = str(irf) + ' '
+    nsim     = str(nsim)  + ' '
+    nlon     = str(nlon) + ' ' 
+    lag      = str(lag) + ' '
+    ng       = str(nyears) + ' '
+    nsgtart  = str(startyear) + ' '
+    iamt     = str(iamt) + ' '
+    ival     = str(ival) + ' '
+    irf      = str(irf) + ' '
     
     # Target station details
-    idx = str(targetidx) + ' '
-    lat = str(targetlat) + ' '
-    lon = str(targetlon) + ' '
-    elev = str(targetelev) + ' '
+    idx    = str(targetidx) + ' '
+    lat    = str(targetlat) + ' '
+    lon    = str(targetlon) + ' '
+    elev   = str(targetelev) + ' '
     dcoast = str(targetdcoast) + ' '
-    anrf = str(targetanrf) + ' '
-    temp = str(targettemp) + ' '
+    anrf   = str(targetanrf) + ' '
+    temp   = str(targettemp) + ' '
 
     copy_fortran_data()
     
@@ -135,44 +140,73 @@ def regionaliseddailysim(nyears, startyear, nsim,
     with open("data_r.dat",'w') as file:
         file.writelines(data_r)
 
-    # Begin simulation
-    idrop =-1
-    kk = 0
-    break_out_flag = False
-
-    while True:
-        regionalised_dailyT.regionalised_daily(idrop=idrop)
-        with open('nearby_station_details.out') as file:
-            print(file.read())
-        nearby = np.genfromtxt('nearby_station_details.out', 
-                                skip_header=6, invalid_raise=False) 
-        while True:
-                try:
-                    idrop = int(input("Do you want to drop any nearby station?\nIf yes, enter station sr no otherwise enter zero: " ))
-                    if idrop < 0:
-                        print("Sorry, no numbers below zero.") 
-                    elif idrop > 0:
-                        if idrop > 5:
-                            print("Sorry, please choose one of the listed nearby station sr nos.")
-                        else:
-                            kk += 1
-                            with open ("drop.out", 'a') as file:
-                                file.write(str(int(kk)).rjust(12)
-                                            + str(int(nearby[idrop-1][1])).rjust(12)+'\n')
-                            idrop = kk
-                            break
-                    else:
-                        regionalised_dailyT.regionalised_daily(idrop=idrop)
-                        break_out_flag = True
-                        break
-                except ValueError:
-                    print("Sorry, invalid input. Please makesure input is integer only.")
-        
-        if break_out_flag:
-            break
+    # Get Nearby Stations
+    param = {}
+    param['nNearStns']       = nstation
+    param['pathStnData']     = 'data/stn_record.csv'
+    param['pathModelCoeffs'] = 'data/daily_logreg_coefs.csv'
+    param['pathDailyData']   = data_path
     
-    if netcdf == True:
-        convertdailync(output_path_txt, output_path_nc, startyear, nyears, nsim, missingDay=-999.9)
-        #remove textfile
-        if os.path.exists(output_path_txt):
-            os.remove(output_path_txt)
+    target = {}
+    target['index']           = idx             
+    target['lat']             = lat               
+    target['lon']             = lon               
+    target['elevation']       = elev              
+    target['distToCoast']     = dcoast       
+    target['annualRainDepth'] = anrf
+    target['temp']            = temp    
+    
+    station(param, target, nAttributes=33,
+        fout=f'nearby_station_details.out'
+    )
+    
+    # Begin simulation
+    regionalised_dailyT4.regionalised_daily(idrop=0)
+    
+    # idrop = 0
+    # kk = 0
+    # break_out_flag = False
+
+    # while True:
+        # regionalised_dailyT.regionalised_daily(idrop=idrop)
+        # with open('nearby_station_details.out') as file:
+            # print(file.read())
+        # nearby = np.genfromtxt('nearby_station_details.out', 
+                                # skip_header=6, invalid_raise=False) 
+        # while True:
+                # try:
+                    # idrop = int(input("Do you want to drop any nearby station?\nIf yes, enter station sr no otherwise enter zero: " ))
+                    # if idrop < 0:
+                        # print("Sorry, no numbers below zero.") 
+                    # elif idrop > 0:
+                        # if idrop > 5:
+                            # print("Sorry, please choose one of the listed nearby station sr nos.")
+                        # else:
+                            # kk += 1
+                            # with open ("drop.out", 'a') as file:
+                                # file.write(str(int(kk)).rjust(12)
+                                            # + str(int(nearby[idrop-1][1])).rjust(12)+'\n')
+                            # idrop = kk
+                            # break
+                    # else:
+                        # regionalised_dailyT.regionalised_daily(idrop=idrop)
+                        # break_out_flag = True
+                        # break
+                # except ValueError:
+                    # print("Sorry, invalid input. Please makesure input is integer only.")
+        
+        # if break_out_flag:
+            # break
+    
+    convertdailync(output_path_txt, output_path_nc, startyear, nyears, nsim, missingDay=-999.9)
+    #remove textfile
+    if os.path.exists(output_path_txt):
+        os.remove(output_path_txt)
+    
+    ## Bias Correct and/or Scale Rainfall
+    daily_rain = xr.open_dataset(output_path_nc)
+    daily_rain['day'] = pd.to_datetime(daily_rain['day'], unit='D', origin='julian')
+    smanrf = daily_rain['rainfall'].resample(day="A").sum().mean()
+    daily_rain['rainfall'] = daily_rain['rainfall'] * (anrf * rm)/smanrf
+    daily_rain.to_netcdf(output_path_nc)
+    
